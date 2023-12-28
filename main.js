22fed73c3b466e7f750d7f5976f35c786b4f5b01
rixkyduwi <55983435+rixkyduwi@users.noreply.github.com>
2023-12-28 15:05:15 +0700

    add many images

--- main.js
+++ main.js
@@ -10,8 +10,7 @@ document.querySelector(".sfw").style.display = "flex"
 document.querySelector(".nsfw").style.display = "none"
 
 formSelect.addEventListener('change', generateWaifu)
-formSelectNsfw.addEventListener('change', generateWaifu)
-
+formSelectNsfw.addEventListener('change', generateWaifu);
 let checkboxNsfw = document.querySelector('input[type=checkbox]');    
 
 nsfwToggle.addEventListener('change', ()=>{
@@ -56,10 +55,42 @@ async function fetchWaifu() {
 
 async function fetchWaifuNsfw() {
 	let category = formSelectNsfw.value;
-	await fetch(`https://api.waifu.pics/nsfw/${category}`)
+	await fetch(`https://api.waifu.pics/many/nsfw/${category}`,{
+		method: 'POST',
+		headers: {
+		  'Content-Type': 'application/json', // Tentukan tipe konten JSON jika mengirim data JSON
+		  // 'Authorization': 'Bearer yourAccessToken', // Sertakan header otorisasi jika diperlukan
+		},
+		body: JSON.stringify({
+			exlude: 'value1'
+		  }), // Ubah objek data menjadi string JSON
+	  })
 		.then(response => response.json())
 		.then(quote =>  {
-			randomImage.src = quote.url
+			console.log(quote)
+			// Buat elemen <img> untuk setiap URL gambar dan tambahkan ke dalam elemen kontainer
+			quote.files.forEach((imageUrl, index) => {
+			// Buat elemen <img>
+			const images = document.querySelector(".images")
+			const image = document.createElement('div');
+			const imgElement = document.createElement('img');
+
+			// Atur atribut src dengan URL gambar
+			imgElement.src = imageUrl;
+			
+			// Tambahkan kelas "image" ke elemen <img>
+  			imgElement.classList.add('random-image');
+			imgElement.classList.add('img-fluid');
+
+			// Atur atribut alt (opsional)
+			imgElement.alt = `Image ${index + 1}`;
+
+			// Tambahkan elemen <img> ke dalam elemen kontainer
+			const br = document.createElement('br');
+			images.appendChild(br);
+			images.appendChild(image);
+			image.appendChild(imgElement);
+			});
 	})
 }
 
